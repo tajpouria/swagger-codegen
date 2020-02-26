f63ad14a94ff00514de6fdd6c8eaea4009e45bd9
tajpouria <tajpouria.dev@gmail.com>
2020-02-26 15:34:10 +0330

    Changed -- worflow test

--- packages/swagger-codegen-cli/src/test.spec.ts
+++ packages/swagger-codegen-cli/src/test.spec.ts
@@ -1,8 +1,5 @@
-import { log } from '@swagger-codegen/core';
 import { add } from '@swagger-codegen/utils';
 
 it('cli test', () => {
-  log('Core log at cli successfully');
-
   expect(add(1, 1)).toBe(2);
 });
